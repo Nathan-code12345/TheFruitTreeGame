70d3af9c03ddb78daf1def3bd938071205b8b241
Nathan-code12345 <83987026+Nathan-code12345@users.noreply.github.com>
2021-07-25 10:32:39 -0700

    Add files via upload

---
+++ Project-C28-template-main/launcher.js
@@ -0,0 +1,30 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.band = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.band);
+    }
+
+    fly(){
+        this.band.bodyA = null;
+    }
+    attach(body){
+        this.band.bodyA=body
+        
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.band.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
+    
+}
